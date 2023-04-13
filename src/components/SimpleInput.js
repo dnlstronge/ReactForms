e231dc8a6e017cc9f2d0027fe6976df1454060db
Dan Stronge <dnlstronge@gmail.com>
2023-04-13 08:40:18 +0100

    [x] - add state and handler

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,16 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const handleEnteredName = (e) => {
+    setEnteredName(e.target.value);
+  };
   return (
     <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input onChange={handleEnteredName} type="text" id="name" />
       </div>
       <div className="form-actions">
         <button>Submit</button>
