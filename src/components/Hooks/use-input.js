6a98a9b2ddad93f592dc903a8f1d6005f139b829
Dan Stronge <dnlstronge@gmail.com>
2023-04-14 08:59:48 +0100

    [x] - 2 X handlers to hook

--- src/components/Hooks/use-input.js
+++ src/components/Hooks/use-input.js
@@ -1,13 +1,30 @@
 import { useState } from "react";
 
 
-const useInput = () => {
-    const [enteredVlaue, setEnteredValue] = useState("");
+const useInput = ( validateValue ) => {
+    const [enteredValue, setEnteredValue] = useState("");
     const [isTouched, setIsTouched] = useState(false);
     
     /* inferred state */
-    const valueIsValid = enteredName.trim() !== "";
-    const hasError = !enteredNameIsValid && enteredNameTouched;
+    const valueIsValid = validateValue(enteredValue) // validtion function
+    const hasError = !valueIsValid && isTouched;
+
+
+  const handleValueChange = (e) => {
+    setEnteredValue(e.target.value);
+  };
+
+  const handleIsTouched = (e) => {
+    setIsTouched(true);
+  };
+
+
+    
+    return {
+        value: enteredValue,
+        hasError,
+
+    }
 }
 
 export default useInput;
